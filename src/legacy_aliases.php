a17403b97859eb34055b5e5fad27e43b22355005
rawp-id <wpipit35@gmail.com>
2026-06-05 08:19:26 +0700

    feat: Implement legacy alias files for framework and server packages

---
+++ src/legacy_aliases.php
@@ -0,0 +1,5 @@
+<?php
+class_alias(\Nexph\Framework\Application::class, 'Nexph\Server\Application');
+class_alias(\Nexph\Server\Http::class, 'Nexph\Server\HttpServer');
+class_alias(\Nexph\Server\Request::class, 'Nexph\Server\ServerRequest');
+class_alias(\Nexph\Server\Http::class, 'Nexph\Server\Response\ServerResponse');
